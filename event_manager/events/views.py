4b01b1a497bcefc3fbc52efceb26cc2656f8cce8
gabrielecorbettabg <gabriele.corbetta.bg@gmail.com>
2020-09-07 11:15:42 +0100

    Register event in admin site

--- event_manager/events/views.py
+++ event_manager/events/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Event
 
 
-def home_page(request):
-    return render(request, 'home.html', {})
+class EventListView(ListView):
+    model = Event
+    template_name = "events/home.html"
+    context_object_name = "events"
+    ordering = ["date"]
+
+
+class EventDetailView(DetailView):
+    model = Event
