b4d1f88bad0d50a9fed03bd2d73f00478cd3c5e5
gabrielecorbettabg <gabriele.corbetta.bg@gmail.com>
2020-09-07 14:12:10 +0100

    Tests event views

--- event_manager/events/tests.py
+++ event_manager/events/tests.py
@@ -1,11 +1,10 @@
-from datetime import timedelta
+from datetime import date, timedelta
 
-from django.utils import timezone
-from django.test import TestCase, SimpleTestCase
+from django.test import TestCase, SimpleTestCase, Client
 from django.contrib.auth.models import User
 from django.urls import reverse, resolve
 
-from .views import EventListView, EventDetailView
+from .views import EventListView, EventDetailView, EventCreateView
 from .models import Event
 
 
@@ -27,7 +26,7 @@ class TestEvent(TestCase):
             name='Test Event',
             venue='London',
             organizer=self.organizer,
-            date=timezone.now() + timedelta(days=10),
+            date=date.today() + timedelta(days=10),
             capacity=1
         )
 
@@ -49,3 +48,74 @@ class TestEventUrls(SimpleTestCase):
     def test_event_detail_url(self):
         url = reverse('event-detail', args=[1])
         self.assertEqual(resolve(url).func.view_class, EventDetailView)
+
+    def test_event_create_url(self):
+        url = reverse('event-create')
+        self.assertEqual(resolve(url).func.view_class, EventCreateView)
+
+
+class TestEventViews(TestCase):
+    def setUp(self):
+        self.organizer = User.objects.create(
+            username='organizer',
+            password='supersecure',
+        )
+        self.attendee = User.objects.create(
+            username='attendee',
+            password='verysafe',
+        )
+        self.organizer.is_active = True
+        self.organizer.save()
+        self.attendee.is_active = True
+        self.attendee.save()
+        self.test_event = Event.objects.create(
+            id=1,
+            name='Test Event',
+            venue='London',
+            organizer=self.organizer,
+            date=date.today() + timedelta(days=10),
+            capacity=1
+        )
+        self.client = Client()
+
+    def test_home_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/home.html')
+        self.assertEqual(len(response.context['events']), 1)
+
+    def test_event_detail_view(self):
+        response = self.client.get(reverse('event-detail', args=[1]))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/event_detail.html')
+        self.assertEqual(response.context['event'].name, 'Test Event')
+
+    def test_event_create_view(self):
+        response = self.client.get(reverse('event-create'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'events/event_form.html')
+
+    def test_event_create_valid(self):
+        self.client.force_login(self.organizer)
+        event_data = {
+            'name': 'Aperitivo',
+            'venue': 'Beach Bar',
+            'capacity': 10,
+            'date': date.today() + timedelta(days=5),
+        }
+        response = self.client.post(reverse('event-create'), event_data)
+        self.assertEqual(response.status_code, 302)
+        event_created = Event.objects.filter(name='Aperitivo').exists()
+        self.assertTrue(event_created)
+
+    def test_event_create_invalid(self):
+        self.client.force_login(self.organizer)
+        event_data = {
+            'name': 'Aperitivo2',
+        }
+        response = self.client.post(reverse('event-create'), event_data)
+        self.assertEqual(response.status_code, 200)
+        form = response.context['form']
+        self.assertFalse(form.is_valid())
+        event_created = Event.objects.filter(name='Aperitivo2').exists()
+        self.assertFalse(event_created)
