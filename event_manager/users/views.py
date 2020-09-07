b95606dd0c03372029498fdbc44d636234085a29
gabrielecorbettabg <gabriele.corbetta.bg@gmail.com>
2020-09-07 12:09:06 +0100

    Register users app

---
+++ event_manager/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Successfully created new account {username}!')
+            return redirect('home')
+    form = UserCreationForm()
+    return render(request, 'users/sign_up.html', {'form': form})
