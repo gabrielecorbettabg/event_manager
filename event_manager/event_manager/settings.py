42b711284a3f56c8de431493e0a9f1f2a0603335
gabrielecorbettabg <gabriele.corbetta.bg@gmail.com>
2020-09-07 10:27:02 +0100

    Base view template, register home page

--- event_manager/event_manager/settings.py
+++ event_manager/event_manager/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'event_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['event_manager/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
